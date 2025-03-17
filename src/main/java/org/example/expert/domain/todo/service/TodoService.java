51d10535fa0ed5fc058455c0e79e597d61a908ed
plusmate <psejin1478@gmail.com>
2025-03-17 15:17:32 +0900

    Lv 2-3 과제 해결(querydsl)

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -18,13 +18,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TodoService {
+
 
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
